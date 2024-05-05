08a0c4c1169647149df0af8f595c47e18d4e6605
ajaysingh <asapsingh7@gmail.com>
2024-05-05 10:55:53 +0530

    Initial commit

---
+++ src/main/java/com/example/demo/model/Test.java
@@ -0,0 +1,14 @@
+package com.example.demo.model;
+
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Data
+public class Test {
+    String name;
+    String val;
+}
